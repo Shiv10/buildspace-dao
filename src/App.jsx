52b76e7238b1783d31746879ad55964ae002a363
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-17 14:15:05 +0530

    fix: heading

--- src/App.jsx
+++ src/App.jsx
@@ -121,7 +121,7 @@ const App = () => {
   if(hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1>🍪DAO Member Page</h1>
+        <h1>Transfers discussion DAO. Hale Fabrizio</h1>
         <p>Congratulations on being a member</p>
         <div>
           <div>
