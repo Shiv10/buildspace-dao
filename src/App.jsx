7db942effa55629c0ccfc9b7caa245aa89b5e368
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-17 14:13:28 +0530

    feat: display token holders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useMemo, useState } from "react";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useWeb3 } from "@3rdweb/hooks";
+import { ethers } from 'ethers';
 
 const sdk = new ThirdwebSDK("rinkeby");
 
 const bundleDropModule = sdk.getBundleDropModule(
   '0xec585f1103706a2bdcc7d4be2a62a6d586d5b3e1',
+);
+
+const tokenModule = sdk.getTokenModule(
+  "0xb7c565C82f30BA6F1A24b155e5D513075b21d358",
 );
 
 const App = () => {
@@ -20,6 +25,58 @@ const App = () => {
   const signer = provider ? provider.getSigner(): undefined;
 
   const [isClaiming, setIsClaiming] = useState(false);
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  const shortenAddress = (str) => {
+    return str.toString().substring(0,6) + "..." + str.toString().substring(str.length - 4);
+  }
+
+  useEffect(async () => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+    
+    // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+    // with tokenId 0.
+    try {
+      const memberAddresses = await bundleDropModule.getAllClaimerAddresses("0");
+      setMemberAddresses(memberAddresses);
+      console.log("🚀 Members addresses", memberAddresses);
+    } catch (error) {
+      console.error("failed to get member list", error);
+    }
+  }, [hasClaimedNFT]);
+
+  useEffect(async () => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+  
+    // Grab all the balances.
+    try {
+      const amounts = await tokenModule.getAllHolderBalances();
+      setMemberTokenAmounts(amounts);
+      console.log("👜 Amounts", amounts);
+    } catch (error) {
+      console.error("failed to get token amounts", error);
+    }
+  }, [hasClaimedNFT]);
+
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          // If the address isn't in memberTokenAmounts, it means they don't
+          // hold any of our token.
+          memberTokenAmounts[address] || 0,
+          18,
+        ),
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
+
 
   useEffect(async () => {
     sdk.setProviderOrSigner(signer);
@@ -64,8 +121,31 @@ const App = () => {
   if(hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1>DAO Member Page</h1>
+        <h1>🍪DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     )
   }
